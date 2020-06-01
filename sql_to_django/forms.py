fc0b0452af1596ad0c2394d0812b9a525e1d330b
rimi <yerimi@kakao.com>
2020-06-01 10:27:41 +0900

    add class table

--- sql_to_django/forms.py
+++ sql_to_django/forms.py
@@ -4,4 +4,5 @@ from sql_to_django.widgets import DivContentEditable
 
 
 class QueryInputForm(forms.Form):
-    query = forms.CharField(max_length=1000, widget=forms.Textarea(attrs={'class': 'form-control'}))
+    fk_check = forms.BooleanField(required=False, initial=False, widget=forms.CheckboxInput(attrs={'class': 'custom-control-input', 'disabled': 'true'}))
+    query = forms.CharField(max_length=1000, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': '정상작동하는 SQL 구문을 넣어주세요.'}))
