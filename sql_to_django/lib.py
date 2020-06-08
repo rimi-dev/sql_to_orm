a419a54be05ab90ddd9bdcb82dbf154c1c81cc85
rimi <yerimi@kakao.com>
2020-06-08 14:19:38 +0900

    [change] lib.py : change def to class

--- sql_to_django/lib.py
+++ sql_to_django/lib.py
@@ -9,78 +9,83 @@ class Table:
     Date : 2020. 05. 27
     Description : get/set main table, get/set joined tables
     """
-    main_table = ''
     main_named_table = ''
-    joined_table = []
+    main_table = ''
+    joined_table = ''
 
     @classmethod
-    def set_main_table(cls, table):
+    def __init__(cls, table):
         if len(table) > 1:
             cls.main_named_table = table[1]
-        cls.main_table = table
-
-    @classmethod
-    def set_joined_table(cls, table, as_name_table):
-        cls.joined_table.append([table, as_name_table])
+        cls.main_table = table[0]
 
     @classmethod
     def get_main_table(cls):
         return cls.main_table
 
     @classmethod
+    def get_main_named_table(cls):
+        return cls.main_named_table
+
+    @classmethod
     def get_joined_table(cls):
         return cls.joined_table
 
-    @classmethod
-    def reset_joined_table(cls):
-        cls.joined_table = []
 
-
-def select_logic(query):
-    query = re.split(r'from', query)
-    target = list_whitespace_remove(query)
-    table_name = target[1].split()
-    table_len = len(table_name)
-    columns = target[0].split(', ')
-    value_columns = ''
-    if table_len > 1:
-        Table.set_main_table(table_name)
-        print(Table.get_main_table())
-        main_table = table_name[1]
-        for item in columns:
-            if main_table in item:
-                if '*' in target[0]:
-                    value_columns += ''
-                else:
-                    value_columns += f'"{item[len(main_table)+1:]}", '
-    else:
-        if '*' in target[0]:
-            value_columns = ''
+class Select:
+    def __init__(self, query):
+        query = re.split(r'from', query)
+        target = list_whitespace_remove(query)
+        table_name = target[1].split()
+        table_len = len(table_name)
+        columns = target[0].split(', ')
+        value_columns = ''
+        if table_len > 1:
+            Table(table_name)
+            main_table_named = Table.get_main_named_table()
+            for item in columns:
+                if main_table_named in item:
+                    if '*' in target[0]:
+                        value_columns += ''
+                    else:
+                        value_columns += f'"{item[len(main_table_named) + 1:]}", '
         else:
-            value_columns = target[0]
-    orm = f'{table_name[0]}.objects.values({value_columns})'
-    return orm
+            if '*' in target[0]:
+                value_columns = ''
+            else:
+                value_columns = target[0]
+        self._orm = f'{Table.get_main_table()}.objects.values({value_columns})'
+
+    def get_orm(self):
+        return self._orm
 
 
-def inner_join_logic(query):
-    return query
+class Join:
+    def inner_join_logic(query):
+        return query
+
+    def left_outer_join_logic(query):
+        return query
 
 
-def left_outer_join_logic(query):
-    return query
+class OrderBy:
+    def __init__(self, query):
+        order_by_query = query.split()
+        len_order_by_query = len(order_by_query)
+        column = order_by_query[0]
+        main_table_named = Table.get_main_named_table()
+        if main_table_named in column:
+            column = column.split('.')[1]
+        # table name 이 있을경우
+        # To sort the records in descending order
+        sort = ''
+        if len_order_by_query > 1:
+            if order_by_query[1].lower() == 'desc':
+                sort = '-'
+        self._orm = f'.order_by("{sort}{column}")'
 
-
-def order_by_logic(query):
-    order_by_query = query.split()
-    len_order_by_query = len(order_by_query)
-    column = order_by_query[0]
-    # table name 이 있을경우
-    # To sort the records in descending order
-    sort = ''
-    if len_order_by_query > 1:
-        if order_by_query[1].lower() == 'desc':
-            sort = '-'
-    return f'.order_by("{sort}{column}")'
+    def get_orm(self):
+        return self._orm
 
 
 def like(column, value):
@@ -97,11 +102,11 @@ def where_logic(query):
 
 class QueryFuncManager(object):
     _queryMappingTable = {
-        "select": select_logic,
+        "select": Select,
         "where": where_logic,
-        "inner join": inner_join_logic,
-        "left outer join": left_outer_join_logic,
-        "order by": order_by_logic,
+        # "inner join": inner_join_logic,
+        # "left outer join": left_outer_join_logic,
+        "order by": OrderBy,
     }
 
     @classmethod
